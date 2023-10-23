6c1afe52a4de652d609df012bf4f4595163f693f
Arif Rabbani <arif.rabbani@rksv.in>
2023-10-23 15:31:15 +0530

    home page developed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [{ path: 'quiz', loadChildren: () => import('./quiz/quiz.module').then(m => m.QuizModule) }];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'quiz',
+    loadChildren: () => import('./quiz/quiz.module').then(m => m.QuizModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
